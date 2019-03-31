88cb510b6392410d837ad6a43a9e330b3e55906d
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-03-31 20:52:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,8 +20,8 @@ def get_user_input(value)
 return get.chomp(value)
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
